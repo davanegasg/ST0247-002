7a9fcbff55e32c6890b843f359bb637287af8b47
jacevar <63611163+jacevar@users.noreply.github.com>
2021-03-01 11:27:07 -0500

    Update Taller4.java

--- talleres/taller04/Taller4.java
+++ talleres/taller04/Taller4.java
@@ -61,7 +61,32 @@ public class Taller4 {
      * @return cual es el costo que tiene ir desde inicio a fin
      */
     public static int costoMinimo(Digraph g, int inicio, int fin) {
+        ArrayList<Integer> next = g.getSuccessors(source);
+        next.remove((Integer)source);
+        
+        checked[source] = true;
+        boolean answer = false;
 
-    }
+        
+        if(destination == source){
+            answer = true;
+        }
+        
+
+        for(int neighbor: next){
+            if(checked[neighbor] == false){
+                Pair<ArrayList<Integer>, Integer> rutas = new Pair<source,destination>();
+                ruta.set(1, ruta.get(1) + g.getWeight(source, neighbor));            
+                rutas[0].append(source);
+                answer =  lowCostPathAux(g, neighbor, destination, checked, ruta);               
+            }
+            if (answer == true){
+              break;
+            }
+        }
+        return answer;
+  }
+
+    
 
 }
