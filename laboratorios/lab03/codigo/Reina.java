14917d0e954ef2598c8a5dbc2ce3c4422812ae18
jacevar <63611163+jacevar@users.noreply.github.com>
2021-03-10 19:50:20 -0500

    Create Reina.java

---
+++ laboratorios/lab03/codigo/Reina.java
@@ -0,0 +1,93 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Clase en la cual se implementan los metodos del Taller 3
+ * 
+ * @author Mauricio Toro, Diego Alejandro Vanegas, Jacobo Rave Londoño
+ */
+public class Main{
+
+    /**
+     * Metodo que verifica si es posible poner las reinas hasta la columna c
+     * 
+     * @param  c hasta esta columna revisa
+     * @param  tablero el tablero
+     * @return true si es posible, false de lo contrario
+     */	
+    public static void main(String args[]){
+
+     boolean a = nReinas(4);
+     System.out.print(a);
+
+    }
+
+
+    private static boolean puedoPonerReina(int c, int[] tablero) {
+        for(int i = 0; i<c ; i++){
+            if(tablero[i]==tablero[c]){
+                return false;
+            }
+            else if(Math.abs(c-i) == Math.abs(tablero[c]-tablero[i])){
+                return false;
+
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Metodo auxiliar para llamar el metodo posterior
+     * 
+     * @param  n numero de reinas
+     * @return numero de soluciones
+     */	
+    public static boolean nReinas(int n) {
+        return nReinas(0, n, new int[n]);
+    }
+
+    /**
+     * Metodo devuelve el numero de soluciones que tiene el problema
+     * 
+     * @param  c columna
+     * @param  n numero de reinas
+     * @return numero de soluciones
+     */	
+    private static boolean nReinas(int c, int n, int[] tablero) {
+        boolean done=false;
+        if(c==n){
+            return true;
+        }
+        else{
+            for(int i = 0 ; i<n ; i++){
+                tablero[c]=i;
+              
+                if(puedoPonerReina(c,tablero)){
+                    done = done || nReinas(c+1,n,tablero);
+
+                }
+                if(done==true){
+                  return done;
+                }
+            }
+            return false;
+        }
+    }
+
+
+    public static void imprimirTablero(int[] tablero) {
+        int n = tablero.length;
+        System.out.print("    ");
+        for (int i = 0; i < n; ++i)
+            System.out.print(i + " ");
+        System.out.println("\n");
+        for (int i = 0; i < n; ++i) {
+            System.out.print(i + "   ");
+            for (int j = 0; j < n; ++j)
+                System.out.print((tablero[i] == j ? "Q" : "#") + " ");
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+}
