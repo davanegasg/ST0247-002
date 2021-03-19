9b4967d8fd601a648d1c860aa2ae4d0edba0c77b
jacevar <63611163+jacevar@users.noreply.github.com>
2021-03-19 10:39:13 -0500

    Update Taller6.java

--- talleres/taller06/Taller6.java
+++ talleres/taller06/Taller6.java
@@ -15,31 +15,61 @@ public class Taller6 {
 	* @return un conjunto de unidades por denominacion
 	*/
 	public static int[] cambioGreedy(int n, int[] denominaciones) {
-		
-	}
-
-	/**
-	* Metodo que recorre todo el grafo con la intencion de buscar un
-	* camino que represente el menor costo pasando por todos los vertices exactamente
-	* una vez y vuelva al nodo inicial
-	* @param g grafo dado 
-	* @return cual es el costo que tiene
-	*/
-	public static int recorrido(Digraph g) {
-	
-	}
-	
-	public static int PesoMinimo(ArrayList<Integer> sucesor, Digraph g, int v, boolean[] visitados){
-        int pesomin = Integer.MAX_VALUE;
-        int nodo = 0;
-        for(int i = 0; i < sucesor.size(); i++){
-            if(visitados[sucesor.get(i)] == false){
-                if(pesomin > g.getWeight(v, sucesor.get(i))){
-                    pesomin = g.getWeight(v, sucesor.get(i));
-                    nodo = sucesores.get(i);
-                }
+        int solucion = 0;
+        int[] res = new int[denominaciones.length]; 
+        int i = 0;
+        while (i <= denominaciones.length-1)
+        {
+            if (solucion == n) return res;
+            else if (denominaciones[i] <= (n-solucion)) {
+                solucion += denominaciones[i];
+                res[i]++;
+            } else {
+                i++;
             }
         }
-        return nodo;
+		return solucion == n? res : null;
+	    }
+	    public static int recorrido(Digraph g) {
+		boolean[] visitados = new boolean[g.size()];
+		int costo = 0;
+		return recorrido(0, g, visitados, costo);
+	    }
+
+
+	    public static int recorrido(int v, Digraph g, boolean[] visitados, int costo){
+		if(ok(visitados)){
+		    return costo + g.getWeight(v,0);
+		}
+		ArrayList<Integer> sucesores = g.getSuccessors(v);
+		int nodo = nodoPesoMinimo(sucesores, g, v, visitados);
+		costo = costo + g.getWeight(v, nodo);
+		visitados[v] = true;
+
+		return recorrido(nodo, g, visitados, costo);
+	    }
+
+
+	    private static boolean ok(boolean[] szs){ 
+		for(int i = 0; i<szs.length; i++){
+		    if(!szs[i])return false;
+		}
+		return true;
+	    }
+
+
+	    public static int nodoPesoMinimo(ArrayList<Integer> sucesores, Digraph g, int v, boolean[] visitados){
+		int pesoMinimo = Integer.MAX_VALUE;
+		int nodo = 0;
+		for(int i = 0; i < sucesores.size(); i++){
+		    if(visitados[sucesores.get(i)] == false){
+			if(pesoMinimo > g.getWeight(v, sucesores.get(i))){
+			    pesoMinimo = g.getWeight(v, sucesores.get(i));
+			    nodo = sucesores.get(i);
+			}
+		    }
+		}
+		return nodo;
+	    }
 
 }
