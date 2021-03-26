74e5dd32455fb641ff35befe3d02734b6a2c2c7c
jacevar <63611163+jacevar@users.noreply.github.com>
2021-03-25 21:10:52 -0500

    Create Lector.java

---
+++ proyecto/codigo/Lector.java
@@ -0,0 +1,145 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Lector{
+    private static Scanner scan;
+    /**
+     * El método leerDatos instancia la clase Dato y lee los datos del archivo uno por uno para
+     * introducirlos en un array nuevo y lo retorna despues.
+     * @return datos (lista de datos)
+     */
+    public static void leer(){
+        ArrayList<cVehicle> datos=new ArrayList<>();
+        cVehicle v;
+        ArrayList<Coordinates> c = new ArrayList<>();
+        Coordinates cor;
+        ArrayList<l> time1 = new ArrayList<>();
+        l timeL;
+        ArrayList<g> levelWatts1 = new ArrayList<>();
+        g LvlW;
+
+        System.out.println("Por favor inserta la ruta del archivo: ");
+        scan = new Scanner(System.in);
+        String ruta = scan.nextLine();//NQ20200429-30.txt
+        File archivo=null;
+        try{
+            archivo= new File(ruta);
+            scan = new Scanner(archivo);
+            ArrayList datosVehic = new ArrayList<>();
+            ArrayList coordi = new ArrayList<>();
+            ArrayList time = new ArrayList<>();
+            ArrayList LevelWatts = new ArrayList<>();
+            int state = 0;
+            while(scan.hasNextLine()){
+                String linea = scan.nextLine();
+                if(linea == ""){
+                    continue;
+                }
+                String arreglo[] = linea.split(" ");
+                switch(state){
+                    case 0:
+                    if(!arreglo[0].equals("") && !arreglo[0].equals("Coordinates")){
+                        datosVehic.add(Double.parseDouble(arreglo[2]));
+                    }
+                    if(arreglo[0].equals("Coordinates")){
+                        v = crearVehiculo(datosVehic);
+                        datos.add(v);
+                        state = 1;
+                    }
+                    break;
+                    case 1: 
+                    while(!linea.equals("") && !linea.equals("l")){
+                        for(int i = 0; i<6; i++){
+                            if(i == 1){
+                                coordi.add(arreglo[i]);
+                            } else if(i == 4){
+                                coordi.add(arreglo[i]);
+                            } else{
+                                coordi.add(Double.parseDouble(arreglo[i]));
+                            }
+                        }
+                        cor = asignarC(coordi);
+                        coordi.clear();
+                        c.add(cor);
+                        break;
+                    }
+                    if(linea.equals("l")){
+                        state = 2;
+                    }
+                    break;
+                    case 2:
+                    if(!linea.equals("")&&!linea.equals("g")){
+                        for(int i = 1; i<4 ;i++){
+                            time.add(Double.parseDouble(arreglo[i]));
+                        }
+                        timeL = asignarL(time);
+                        time.clear();
+                        time1.add(timeL);
+                    }
+                    if(linea.equals("g")){
+                        state = 3;
+                    }
+                    break;
+                    case 3:
+                    if(!linea.equals("")){
+                        for(int i = 1; i<4 ;i++){
+                            LevelWatts.add(arreglo[i]);
+                        }
+                        LvlW = asignarG(LevelWatts);
+                        LevelWatts.clear();
+                        levelWatts1.add(LvlW);
+                    }
+                    break;
+                }
+            }
+        }catch(FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static cVehicle crearVehiculo(ArrayList<Double> datosVehic){
+        int n =  Integer.parseInt(String.valueOf(datosVehic.get(0)).replace(".0", ""));
+        int m = Integer.parseInt(String.valueOf(datosVehic.get(1)).replace(".0", ""));
+        int u = Integer.parseInt(String.valueOf(datosVehic.get(2)).replace(".0", ""));
+        int breaks = Integer.parseInt(String.valueOf(datosVehic.get(3)).replace(".0", ""));
+        double r = datosVehic.get(4);
+        double speed = datosVehic.get(5);
+        double Tmax = datosVehic.get(6);
+        double Smax = datosVehic.get(7);
+        double st_customer = datosVehic.get(8);
+        double Q = datosVehic.get(9);
+
+        cVehicle cVehiculo = new cVehicle(n, m, u, breaks, r, speed, Tmax, Smax, st_customer, Q);
+        return cVehiculo;
+    }
+
+    public static Coordinates asignarC(ArrayList<Integer> coordi){
+        int idNode = Integer.parseInt(String.valueOf(coordi.get(0)).replace(".0", ""));
+        String nNode = String.valueOf(coordi.get(1));   
+        double x = Double.parseDouble(String.valueOf(coordi.get(2))); 
+        double y = Double.parseDouble(String.valueOf(coordi.get(3))); 
+        String tNode = String.valueOf(coordi.get(4));  
+        int tStation = Integer.parseInt(String.valueOf(coordi.get(5)).replace(".0", ""));
+
+        Coordinates Coordenadas = new Coordinates(idNode, nNode, x, y, tNode, tStation);
+        return Coordenadas;
+    }
+
+    public static l asignarL(ArrayList<Double> time){
+        double tSlow = Double.parseDouble(String.valueOf(time.get(0)));
+        double tMedium = Double.parseDouble(String.valueOf(time.get(1)));
+        double tFast = Double.parseDouble(String.valueOf(time.get(2)));
+        l Tiempos = new l(tSlow, tMedium, tFast);
+        return Tiempos;
+    }
+    public static g asignarG(ArrayList<Integer> levelWatts){
+        int bSlow = Integer.parseInt(String.valueOf(levelWatts.get(0)).replace(".0", ""));
+        int bMedium = Integer.parseInt(String.valueOf(levelWatts.get(1)).replace(".0", ""));
+        int bFast = Integer.parseInt(String.valueOf(levelWatts.get(2)).replace(".0", ""));
+        g Niveles = new g(bSlow, bMedium, bFast);
+        return Niveles;
+    }
+}
