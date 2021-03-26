d590f2418b35614da33a68cc671228908bd7fcff
jacevar <63611163+jacevar@users.noreply.github.com>
2021-03-25 21:13:15 -0500

    Create Coordinates.java

---
+++ proyecto/codigo/Coordinates.java
@@ -0,0 +1,53 @@
+import java.util.*;
+/**
+ * Write a description of class Coordinates here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Coordinates
+{
+    int idNode; //Id del nodo
+    String nNode; //Nombre del nodo
+    double x; //Coordenadas en X
+    double y; //Coordenadas en Y
+    String tNode; //Tipo de nodo
+    int tStation; //Tipo de estación
+
+    public Coordinates(int idNode, String nNode, double x, double y, String tNode, int tStation){
+        this.idNode = idNode; 
+        this.nNode = nNode;   
+        this.x = x;  
+        this.y = y;  
+        this.tNode = tNode;  
+        this.tStation = tStation; 
+    }
+
+    public String toStringg(){
+        return (idNode + " " + nNode + " " + x + " " + y + " " + tNode + " " + tStation);
+    }
+
+    public int getIdNode(){
+      return idNode;
+    }
+
+    public String getNnode(){
+      return nNode;
+    }
+
+    public double getX(){
+      return x;
+    }
+
+    public double getY(){
+      return y;
+    }
+    
+    public String getTnode(){
+      return tNode;
+    }
+
+    public int getTstation(){
+      return tStation;
+    }
+}
